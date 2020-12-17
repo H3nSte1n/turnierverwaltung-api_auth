8fbdd2a41df8aa64f9e28331c8f79804cec379da
Henry Steinhauer <henry.steinhauer@i22.de>
2020-12-17 13:50:40 +0100

    feat: Testing swagger documentation is available

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -1,16 +1,5 @@
 package com.turnierverwaltung_api_auth
 
-import io.ktor.http.*
-import io.ktor.server.testing.*
-import kotlin.test.*
-
 class ApplicationTest {
-    @Test
-    fun testRoot() {
-        withTestApplication({ module() }) {
-            handleRequest(HttpMethod.Get, "/").apply {
-                assertEquals(HttpStatusCode.OK, response.status())
-            }
-        }
-    }
+
 }
