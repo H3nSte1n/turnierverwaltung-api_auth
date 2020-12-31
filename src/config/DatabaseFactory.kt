ab3df60d833d973a94cff4b081840fdf1d386a1e
Henry Steinhauer <henry.steinhauer@i22.de>
2020-12-31 18:58:16 +0100

    fix: Test edit workflow

--- src/config/DatabaseFactory.kt
+++ src/config/DatabaseFactory.kt
@@ -32,7 +32,7 @@ object DatabaseFactory {
         val props = Properties()
         props.setProperty("dataSourceClassName", "org.postgresql.ds.PGSimpleDataSource")
         setProperty(props, "user", dotenv["USER"] ?: "root")
-        setProperty(props, "password", dotenv["PASSWORD"] ?: "")
+        setProperty(props, "password", dotenv["PASSWORD"] ?: "secret")
         setProperty(props, "databaseName", dotenv["DATABASE"] ?: "authentication")
         setProperty(props, "portNumber", dotenv["PORT"] ?: "5432")
         setProperty(props, "serverName", dotenv["SERVER"] ?: "localhost")
